cd5b548827e5b2b1d44abedf837e1ae28ea151ff
vpritamani <44382864+vpritamani@users.noreply.github.com>
2020-12-16 08:34:13 -0800

    Cleaned up ReturnList code

--- FamilyMain.java
+++ FamilyMain.java
@@ -10,11 +10,19 @@ public class FamilyMain {
     static int numberOfIndents = 1;
 
     public static void main(String[] args) throws IOException {
+        // intro message
         System.out.println("This program will show you all of the ancestors and descendants of a person.");
+
+        // ask for file
         System.out.print("What is the input file?");
+
+        // initialize scanner and set file equal to the input from scanner
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
         File file = new File(input);
+
+        // Ask for person's name. If the user asks to quit, then quit
+        // else, set the person's name we are looking for to the input
         System.out.print("\n Person's name ('quit' to end)?");
         String str = scanner.nextLine();
         String person = "";
@@ -23,30 +31,31 @@ public class FamilyMain {
         } else {
             person = str;
         }
+
+        // initialize new scanner and FamilyInfo object
         Scanner fileReader = new Scanner(file);
         FamilyInfo familyinfo = new FamilyInfo();
+
+        // read the file and use the read method from familyinfo
         familyinfo.read(fileReader);
 
         Person persona;
         for (Person persons : familyinfo.people) {
+            // if person in our arraylist equals name of inputted person,
+            // then print out their ancestors and descendants
             if (persons.getName().equalsIgnoreCase(person)) {
                 persona = persons;
                 System.out.println("Ancestors: ");
                 System.out.println("   " + persons.getName());
-                for (Person thingies : returnlist(persona.parents, "ancestors")){
-                    //System.out.println(thingies.getName());
-                }
+                returnlist(persona.parents, "ancestors");
                 System.out.println("Descendants: ");
                 System.out.println("   " + persons.getName());
-                for (Person thingies : returnlist(persona.kids, "descendants")){
-                    //System.out.println(thingies.getName());
-                }
-                //System.out.println(returnlist(persona.kids));
+                returnlist(persona.kids, "descendants");
             }
         }
     }
 
-    public static ArrayList<Person> returnlist (ArrayList<Person> list, String type){
+    public static void returnlist (ArrayList<Person> list, String type){
         numberOfIndents++;
         for (Person persons : list){ //Henry VII,
             for (int k = 0; k < numberOfIndents; k++){
@@ -61,7 +70,6 @@ public class FamilyMain {
             }
         }
         numberOfIndents--;
-        return list;
     }
 
     public static ArrayList<String> makeReadableList (ArrayList<Person> list){
